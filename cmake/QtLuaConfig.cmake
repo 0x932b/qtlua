1ef37d4c0870121e1770ed9d8fd353f3ba2c6258
Cheng-Hsi (James) Chao <dev@chenghsi.com>
2025-04-12 16:06:01 -0400

    [NEW] Add Qt6 Build Configuration Files
    Add CMake configuration for Qt6 build
    Add Qt Lua configuration header

---
+++ cmake/QtLuaConfig.cmake
@@ -0,0 +1,9 @@
+# This file was automatically generated.
+# See /Users/james/torch/qtlua/qtlua/CMakeLists.txt
+
+SET(QTLUA_FOUND ON)
+SET(QTLUA_INCLUDE_DIR "/Users/james/torch/qtlua/qtlua")
+SET(QTLUA_DEFINITIONS "")
+SET(QTLUA_LIBRARIES "/Users/james/torch/qtlua/$<TARGET_FILE_NAME:libqtlua>")
+
+
